36bf89a63e78e5a5e93a38550c0d48181f2b6ef0
window1 <doserack@gmail.com>
2023-10-26 12:16:20 +0900

    Initial Commit

---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by window1 on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
